d201b7e15e1388ab11eea28dabe14e52c135bb8d
WeatherboySuper <SuperPlays1020@gmail.com>
2024-03-03 11:52:46 -0600

    Multiple Outlooks Bug Fix

--- Categorial_Outlook_Display.py
+++ Categorial_Outlook_Display.py
@@ -12,7 +12,6 @@ import customtkinter as ctk
 import contextily as ctx
 import sys
 import logging as log
-import warnings
 
 # Import specific functions from modules
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
@@ -23,9 +22,6 @@ from tkinter import messagebox
 from tkinter import TclError
 
 matplotlib.use('TkAgg')
-
-# Suppress the UserWarning about starting a Matplotlib GUI outside of the main thread
-warnings.filterwarnings("ignore", category=UserWarning, module="matplotlib")
 
 # Set up important program wide variables
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -73,6 +69,15 @@ def fetch_d1_cat_outlook():
     outlook_data = response.json()
     return outlook_data # Returns the data from the GeoJSON URL
 
+# Day 2 Categorial
+def fetch_d2_cat_outlook():
+    log.info('Fetching D2 Cat Outlook')
+    url = 'https://www.spc.noaa.gov/products/outlook/day2otlk_cat.nolyr.geojson'
+    response = requests.get(url)
+    response.raise_for_status()
+    outlook_data = response.json()
+    return outlook_data
+
 
 ### Functions to Set Up the Program ###
 
@@ -85,8 +90,6 @@ def create_output_directory(current_directory):
 
 def setup_plot():
     log.info('running setup_plot')
-    # Suppress the UserWarning about starting a Matplotlib GUI outside of the main thread
-    warnings.filterwarnings("ignore", category=UserWarning)
     fig, ax = plt.subplots(figsize=(10,8)) # Set the size of the plot
     fig.patch.set_facecolor('black') # Set the background color of the plot
     ax.set_aspect('equal', adjustable='box')
@@ -164,7 +167,7 @@ def check_outlook_availability(outlook_data):
     return False
 
 # Function to plot the polygons
-def plot_outlook_polygons(ax, outlook_data):
+def plot_cat_outlook_polygons(ax, outlook_data):
     log.info('running plot_outlook_polygons')
     for feature in outlook_data['features']: 
         outlook_type = feature['properties']['LABEL']
@@ -185,13 +188,17 @@ def no_outlook_available():
 ### Functions to Plot and Save the Outlook Image
 
 # Function to display the outlook
-def display_d1_cat_outlook(current_directory, outlook_data):
+def display_cat_outlook(current_directory, outlook_data):
     log.info('running display_outlook')
     fig, ax = setup_plot()
 
+    # Clear the figure and axes before displaying a new outlook
+    fig.clear()
+    ax = fig.add_subplot(111)
+
     if not check_outlook_availability(outlook_data):
         no_outlook_available()
-        return
+        sys.exit(0)
 
     overlay_us_state_outline(ax, current_directory)
     overlay_us_interstate_lines(ax, current_directory)
@@ -201,20 +208,19 @@ def display_d1_cat_outlook(current_directory, outlook_data):
     remove_axes_labels_boxes_title(ax)
     add_header_image(ax, current_directory)
 
-    plot_outlook_polygons(ax, outlook_data)
+    plot_cat_outlook_polygons(ax, outlook_data)
 
     output_directory = create_output_directory(current_directory)
     output_filename = 'spc_cat_outlook.png'
     output_path = os.path.join(output_directory, output_filename)
 
-    log.info('print1')
+    for widget in root.winfo_children():
+        widget.destroy()
 
     # Create a canvas and add it to the root window
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    log.info('print2')
 
     # Create a custom toolbar with a close button
     toolbar = NavigationToolbar2Tk(canvas, root)
@@ -222,20 +228,17 @@ def display_d1_cat_outlook(current_directory, outlook_data):
 
     def close_figure():
         plt.close(fig)
-        root.destroy()
-        root.quit()
+        root.withdraw()
         start_gui()
 
     close_button = tk.Button(toolbar, text='Close', command=close_figure)
     close_button.pack(side=tk.RIGHT)
 
-    log.info('print3')
+    root.protocol("WM_DELETE_WINDOW", close_figure)
 
     # Show the Tkinter root window with the canvas and toolbar
     root.deiconify()
     root.mainloop()
-
-    log.info('print4')
 
     log.info('Showing the plot')
     plt.savefig(output_path, dpi=96, bbox_inches='tight')
@@ -278,8 +281,8 @@ def start_gui():
     window.title('Severe Weather Outlook Display')
 
     # Configure Layout
-    window.grid_columnconfigure(1, weight=1)
-    window.grid_rowconfigure(5, weight=1)
+    window.grid_columnconfigure(5, weight=1)
+    window.grid_rowconfigure(7, weight=1)
 
     # Fonts
     Title_Font = ctk.CTkFont(family='Montserrat', size=40, weight='bold')
@@ -287,31 +290,49 @@ def start_gui():
 
     # Title Label
     Title_Label = ctk.CTkLabel(window, text='Severe Weather Outlook Display', font=Title_Font)
-    Title_Label.grid(columnspan=2)
+    Title_Label.grid(columnspan=7)
 
     # Welcome Label
     Welcome_Label = ctk.CTkLabel(window, text='Welcome to the Severe Weather Outlook Display! Press a button below to find a outlook to dispaly.', font=Description_Font)
-    Welcome_Label.grid(columnspan=2)
+    Welcome_Label.grid(columnspan=7)
 
-    def close_and_run_program():
+    def D1_C_and_R():
         log.info('GUI - running the close_and_run_program function')
         window.withdraw()
         run_d1_cat()
+
+    def D2_C_and_R():
+        log.info('GUI - running the close_and_run_program function')
+        window.withdraw()
+        run_d2_cat()
+
+    def Test_C_and_R():
+        log.info('GUI - running the close_and_run_program function')
+        window.withdraw()
+        run_test_cat()
     
     def close_program():
         log.info('GUI - Now Closing Program')
-        window.destroy() 
+        window.withdraw() 
         sys.exit(0)
 
     window.protocol("WM_DELETE_WINDOW", close_program)
 
     # Day 1 Categorial Button
-    Test_Button = ctk.CTkButton(window, text='Day 1 Categorial', width=300, font=Description_Font, command=close_and_run_program)
-    Test_Button.grid(row=5, column=0, columnspan=1, padx=25, sticky='ew')
+    D1_Cat_Button = ctk.CTkButton(window, text='Day 1 Categorial', width=150, font=Description_Font, command=D1_C_and_R)
+    D1_Cat_Button.grid(row=5, column=0, columnspan=1, padx=25, sticky='ew')
+
+    # Day 2 Categorial Button
+    D2_Cat_Button = ctk.CTkButton(window, text='Day 2 Categorial', width=150, font=Description_Font, command=D2_C_and_R)
+    D2_Cat_Button.grid(row=5, column=1, columnspan=1, padx=25, sticky='ew')
+
+    # Test Categorial Button
+    Test_Button = ctk.CTkButton(window, text='Test Categorial', width=150, font=Description_Font, command=Test_C_and_R)
+    Test_Button.grid(row=5, column=2, columnspan=1, padx=25, sticky='ew')
 
     # Close Button
     Close_Button=ctk.CTkButton(window, text='Close', font=Description_Font, width=50, command=close_program)
-    Close_Button.grid(row=0, column=1, sticky='e')
+    Close_Button.grid(row=0, column=5, sticky='e')
 
     log.info('GUI - Created widgets')
 
@@ -325,6 +346,18 @@ def run_d1_cat():
     log.info('Running Day 1 Categorial Function')
     show_popup('program is Running', 'Program is now running and may take some time to run. Click "ok" or Close to continue')
     outlook_data = fetch_d1_cat_outlook()
-    display_d1_cat_outlook(current_directory, outlook_data)
+    display_cat_outlook(current_directory, outlook_data)
+
+def run_test_cat():
+    log.info('Running Test Categorial')
+    show_popup('program is running', 'Program is now running and may take some time to run. Click "ok" or Close to continue')
+    outlook_data = fetch_test_cat_outlook()
+    display_cat_outlook(current_directory, outlook_data)
+
+def run_d2_cat():
+    log.info('Running Test Categorial')
+    show_popup('program is running', 'Program is now running and may take some time to run. Click "ok" or Close to continue')
+    outlook_data = fetch_d2_cat_outlook()
+    display_cat_outlook(current_directory, outlook_data)
 
 start_gui()
